13adc502e31e285e8dfeac1112270a624d7c5f99
Ujjwal <ujjwaldhanfhole1@gmail.com>
2024-02-02 01:46:23 +0530

    ep1-ep2

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const parent = React.createElement('div', { id: 'parent' },
+    React.createElement('div', { id: 'child' },
+        [React.createElement('h1', { id: 'h1-tag' }, 'I am an h1 tag'), React.createElement('h2', { id: 'h2-tag' }, 'I am an h2 tag')]
+    )
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
